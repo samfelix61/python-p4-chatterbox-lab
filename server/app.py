bfe13e08f6d94a1ccf3fd5dd1585182ba4c8b2f2
samfelix61 <samfelix61@gmail.com>
2024-07-06 17:08:41 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -14,13 +14,54 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        messages_serialized = [m.to_dict() for m in messages]
+        return make_response(messages_serialized, 200)
+    elif request.method == 'POST':
+        body = request.json.get('body')
+        username = request.json.get('username')
 
-@app.route('/messages/<int:id>')
+        if not body or not username:
+            return make_response({'error': 'Missing body or username'}, 400)
+
+        message = Message(body=body, username=username)
+        db.session.add(message)
+        db.session.commit()
+
+        return make_response(message.to_dict(), 201)
+    
+
+@app.route('/messages/<int:id>', methods=['GET','PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first() 
+    if request.method == 'PATCH': 
+
+        if not message:
+            return make_response({'error': 'Message not found'}, 404)
+
+        body = request.json.get('body')
+        username = request.json.get('username')
+
+        if body:
+            message.body = body
+        if username:
+            message.username = username
+
+        db.session.commit()
+
+        return make_response(message.to_dict(), 200)
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_message": True,
+            "message": "Message delted."
+        }
+        return make_response(response_body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
